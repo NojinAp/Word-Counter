23629944a4b2fe63008ca0584504684db401c053
Nojin <nojinap@gmail.com>
2022-04-29 17:24:17 -0400

    Add files via upload

---
+++ WordStat.cpp
@@ -0,0 +1,24 @@
+/*
+	==================================================
+	Word Counter
+	==================================================
+	Name   : Nozhin Azarpanah
+	Date   : January 14, 2022
+*/
+
+#include <iostream>
+
+#include "Word.h"
+
+using namespace sdds;
+using namespace std;
+
+int main()
+{
+	char filename[256];
+	programTitle();
+	cout << "Enter filename: ";
+	cin >> filename;
+	wordStats(filename);
+	return 0;
+}
